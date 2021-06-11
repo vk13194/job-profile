81bcb97189dcc154a1f02ee474384273ff4b04e2
vk13194 <vijaym13194@gmail.com>
2021-06-11 15:07:02 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// BACKEND ENDPOINT BASE URL
-console.log(process.env.REACT_APP_API_URL);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
